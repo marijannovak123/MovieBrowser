321420e21a712f1c64180d595967b36b777ec828
Marijan <marijannovak123@gmail.com>
2019-03-23 23:31:47 +0100

    defaults, implement chained rx requests with all errors caught and not breaking chain

---
+++ MovieBrowser/Data/Models/Responses/BaseResponse.swift
@@ -0,0 +1,9 @@
+//
+//  BaseResponse.swift
+//  MovieBrowser
+//
+//  Created by UHP Mac on 23/03/2019.
+//  Copyright © 2019 Novak. All rights reserved.
+//
+
+import Foundation
---
+++ MovieBrowser/Data/Models/Responses/TokenResponse.swift
@@ -0,0 +1,9 @@
+//
+//  TokenResponse.swift
+//  MovieBrowser
+//
+//  Created by UHP Mac on 23/03/2019.
+//  Copyright © 2019 Novak. All rights reserved.
+//
+
+import Foundation
